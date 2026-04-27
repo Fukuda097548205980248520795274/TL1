091c0c004705f0d0df2b352cf10418e30b9f157e
Fukuda097548205980248520795274 <k024g0083@g.neec.ac.jp>
2026-04-27 14:56:06 +0900

    誤字を修正した。

--- __init__.py
+++ __init__.py
@@ -45,10 +45,10 @@ class MYADDON_OT_stretch_vertex(bpy.types.Operator):
     
 # オペレータ ICO球を生成する
 class MYADDON_OT_create_ico_sphere(bpy.types.Operator):
-    bl_idname = "myaddon.myaddon_ot_create_ico_sphere"
+    bl_idname = "myaddon.myaddon_ot_create_object"
     bl_label = "ICO球生成"
     bl_description = "ICO球を生成する"
-    bl_optioins = {'REGISTER','UNDO'}
+    bl_options = {'REGISTER','UNDO'}
 
     # 実行関数
     def execute(self, context):
